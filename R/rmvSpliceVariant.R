11a7914d0b554169676db0ad97d49b5087d55150
Anand K Gavai <anand.gavai@bioinformatics.nl>
2014-11-05 17:10:12 +0000

    Removed all splice variants exceeding 10.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiGGR@96527 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rmvSpliceVariant.R
+++ R/rmvSpliceVariant.R
@@ -2,6 +2,6 @@ rmvSpliceVariant<-function(gene.info){
 gene.info$GPR<-gsub(" ","",gene.info$GPR,fixed=TRUE)
 gene.info$GPR<-gsub("(","",gene.info$GPR,fixed=TRUE)
 gene.info$GPR<-gsub(")","",gene.info$GPR,fixed=TRUE)
-gene.info$GPR<-gsub("\\.[0-9]","",gene.info$GPR)
+gene.info$GPR<-gsub("\\.[0-9]+","",gene.info$GPR)
 return(gene.info)
 }
